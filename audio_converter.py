d760015707b2036e0448f6a412a66d5b468ed8c0
sourceSmallTree <15175388508@163.com>
2020-07-17 15:07:52 +0800

    converter tool for audio: format and sampling rate

--- audio_converter.py
+++ audio_converter.py
@@ -2,29 +2,36 @@ from ffmpy import FFmpeg
 import os
 
 
-def other_to_wav_int16(src_dir, dst_dir, aim_format):
+def other_to_wav_int16(src_dir, dst_dir, aim_format, sampling_rate):
     """
     将其他格式的音频文件转化为wav格式且int16数据类型、单声道、16000Hz采样率
     root： 父级目录
     dirs：root中每一个文件夹的名字
     files：所有文件的文件名
     """
+    settings = []
+    if sampling_rate != '请选择':
+        settings.append('-ar')
+        settings.append(sampling_rate[:-2])
+
     for root, dirs, files in os.walk(src_dir):
         for name in files:
             # print(os.path.join(root, name))    # 打印出src_dir目录下所有的文件的绝对路径名
             audio_name_src = os.path.join(root, name)
-            print(audio_name_src)
-            audio_name_dst = os.path.join(dst_dir, name.split('.')[0] + '.' + aim_format)
-            print(audio_name_dst)
 
-            # 将other格式文件转化为‘pcm-16位 float、1通道、16000Hz采样率的wav文件'
+            if aim_format == '请选择':
+                audio_name_dst = os.path.join(dst_dir, name)
+            else:
+                audio_name_dst = os.path.join(dst_dir, name.split('.')[0] + '.' + aim_format)
+
+            # 将other格式文件转化为‘pcm-16位 float、1通道、16000Hz采样率的wav文件: ['-acodec', 'pcm_s16le', '-ac', '1', '-ar', '16000']'
             ff = FFmpeg(
                 inputs={audio_name_src: None},
-                outputs={audio_name_dst: ['-acodec', 'pcm_s16le', '-ac', '1', '-ar', '16000']}
+                outputs={audio_name_dst: settings}
             )
             ff.run()
 
 
 if __name__ == '__main__':
     other_to_wav_int16('/Users/uniubi/PycharmProjects/audio_converter_kit/data',
-                       '/Users/uniubi/PycharmProjects/audio_converter_kit/data')
+                       '/Users/uniubi/Desktop/dudu', 'mp3', '44100HZ')
--- windows.py
+++ windows.py
@@ -20,7 +20,7 @@ def select_direction_path2():
 # 点击提交执行的功能
 def execute():
     hint.set('converting . . .')
-    audio_converter.other_to_wav_int16(path_in.get(), path_out.get(), transfer_format.get())
+    audio_converter.other_to_wav_int16(path_in.get(), path_out.get(), transfer_format.get(), sampling_rate_value.get())
     print(path_in.get())
     print(path_out.get())
     hint.set('convert finished')
@@ -29,40 +29,46 @@ def execute():
 # 生成窗口
 window = tk.Tk()
 window.title('批量文件格式转换')
-window.geometry('450x300')
+window.geometry('600x450')
 
 tk.Label(window, text='格式转换').place(x=200, y=10)
 
 # 输入文件夹：输入框及上传按钮
 path_in = tk.StringVar()  # 定义变量
-path_in.set(r'/Users/uniubi/PycharmProjects/audio_converter_kit/data')
-label1 = tk.Label(window, text='输入文件夹:').place(x=50, y=50)
+# path_in.set(r'/Users/uniubi/PycharmProjects/audio_converter_kit/data')
+label1 = tk.Label(window, text='Source folder:').place(x=50, y=50)
 entry1 = tk.Entry(window, textvariable=path_in).place(x=160, y=50)
-btn1 = tk.Button(window, text='上传地址', command=select_direction_path1).place(x=360, y=53)  # 按钮
+btn1 = tk.Button(window, text='Upload', command=select_direction_path1).place(x=360, y=53)  # 按钮
 
 # 目标地址：输入框及上传按钮
 path_out = tk.StringVar()
-path_out.set(r'/Users/uniubi/Desktop/dudu')
-label2 = tk.Label(window, text='输出文件夹:').place(x=50, y=100)  # 从左偏，从上偏
+# path_out.set(r'/Users/uniubi/Desktop/dudu')
+label2 = tk.Label(window, text='Target path:').place(x=50, y=100)  # 从左偏，从上偏
 entry2 = tk.Entry(window, textvariable=path_out).place(x=160, y=100)
-btn2 = tk.Button(window, text='上传地址', command=select_direction_path2).place(x=360, y=103)
+btn2 = tk.Button(window, text='Upload', command=select_direction_path2).place(x=360, y=103)
 
 # 转换的格式
-# aim_f = tk.StringVar()
-# label3 = tk.Label(window, text='目标格式:').place(x=50, y=150)  # 从左偏，从上偏
-# entry4 = tk.Entry(window, textvariable=aim_f).place(x=160, y=150)
-
+options_format = ['wav', 'mp3', 'wma', 'm4a']
 transfer_format = tk.StringVar()
-normal_ddl = Label(window, text='转换格式：').place(x=50, y=150)
-ddl = tk.OptionMenu(window, transfer_format, 'wav', 'mp3').place(x=160, y=150)
+transfer_format.set('Please choose')
+format_label = Label(window, text='Format：').place(x=50, y=150)
+format_selector = tk.OptionMenu(window, transfer_format, *options_format).place(x=130, y=150)
 
 
-submit_btn = tk.Button(window, text='  提 交  ', command=execute).place(x=200, y=190)
+# 采样率
+options_sampling_rate = ['8000HZ', '11025HZ', '24000HZ', '32000HZ', '44100HZ']
+sampling_rate_value = tk.StringVar()
+sampling_rate_value.set('Please choose')
+sampling_rate_l = Label(window, text='Sampling rate：').place(x=50, y=200)
+sampling_rate_selector = tk.OptionMenu(window, sampling_rate_value, *options_sampling_rate).place(x=180, y=200)
+
+
+submit_btn = tk.Button(window, text='  Submit  ', command=execute).place(x=200, y=250)
 
 # 提示区域
 hint = tk.StringVar()
 hint.set('')
-tk.Label(window, textvariable=hint).place(x=190, y=240)
+tk.Label(window, textvariable=hint).place(x=140, y=280)
 
 # 不停的刷新显示
 window.mainloop()
